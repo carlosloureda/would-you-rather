cc0d1c30c205bbf051660e34e6c7d524ad9c4ac4
Carlos Loureda <balanzeneto@gmail.com>
2020-04-12 17:12:08 +0200

    [feat] Implemented basic routing and navigation between pages

--- src/components/NewQuestion/NewQuestion.js
+++ src/components/NewQuestion/NewQuestion.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const NewQuestion = () => {
-  return <h1>Hi from NewQuestion page!</h1>;
+  const history = useHistory();
+
+  const onCreateQuestion = () => {
+    console.log("question submitted");
+    history.push("/");
+  };
+  return (
+    <div>
+      <h1>Hi from NewQuestion page!</h1>
+      <button onClick={onCreateQuestion}>Create</button>
+    </div>
+  );
 };
 
 export default NewQuestion;
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 import AnswerQuestion from "./AnswerQuestion";
 import QuestionDetail from "./QuestionDetail";
 
 const Question = () => {
+  const { id } = useParams();
   return (
     <div>
       <h1>Hi from Question page!</h1>
+      {id && <span>The question id is : {id}</span>}
       <AnswerQuestion />
       <QuestionDetail />
     </div>
